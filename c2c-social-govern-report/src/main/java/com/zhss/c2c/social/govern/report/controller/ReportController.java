f638a9ae387ee8f9b5c6c863c9e8bc2a8188d4bd
liaolingjian <liaolingjian@163.com>
2021-09-04 23:13:54 +0800

    commit master bug

--- c2c-social-govern-report/src/main/java/com/zhss/c2c/social/govern/report/controller/ReportController.java
+++ c2c-social-govern-report/src/main/java/com/zhss/c2c/social/govern/report/controller/ReportController.java
@@ -76,7 +76,7 @@ public class ReportController {
         // 在本地数据库增加一个举报任务
         reportTaskService.add(reportTask);
 
-        ConcurrentHashMap
+       // ConcurrentHashMap
 
         List<Long> reviewerIds = null;
 
